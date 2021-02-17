d7c65be1ef6405f5d2612043ee368bfe8f2fb66c
zhangbao <zhangbaohpu@163.com>
2021-02-17 23:11:56 +0800

    代理，单例

---
+++ src/main/java/com/zhangbao/zhangbaoTest/modules/Test.java
@@ -0,0 +1,13 @@
+package com.zhangbao.zhangbaoTest.modules;
+
+
+/**
+ * @author zhangbao
+ * @date 2021/2/17 11:51
+ **/
+public class Test {
+    public static void main(String[] args) {
+
+    }
+
+}
